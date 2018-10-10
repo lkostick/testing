deaf11f6f3751e9c1ca664925bee4406702b15e0
lkostick <lkostick1@gmail.com>
2018-10-09 23:26:41 -0400

    onfocus

--- level1.js
+++ level1.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     console.log("hello world");
 //    $("#form1").focus(function() {
-    $("#form1").on("focus", function() {
-        //$(this).value='';
-        console.log("form focused");
-    });
+//    $("#form1").on("focus", function() {
+//        //$(this).value='';
+//        console.log("form focused");
+//    });
+});
+$("#form1").on("focus", function() {
+    //$(this).value='';
+    console.log("form focused");
 });
 
