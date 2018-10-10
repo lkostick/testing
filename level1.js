29edc9a552fbc04429e8ed7d20838f4de6abd1da
lkostick <lkostick1@gmail.com>
2018-10-09 23:23:30 -0400

    onfocus

--- level1.js
+++ level1.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     console.log("hello world");
-    $("#form1").focus(function() {
+//    $("#form1").focus(function() {
+    $("#form1").on("focus", function() {
         //$(this).value='';
         console.log("form focused");
     });
