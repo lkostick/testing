adc6a38def5e7ed5fa10c3b01b9fa111fc229939
lkostick <lkostick1@gmail.com>
2018-10-09 23:32:15 -0400

    without jquery

--- level1.js
+++ level1.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
-    console.log("hello world");
-//    $("#form1").focus(function() {
-//    $("#form1").on("focus", function() {
-//        //$(this).value='';
-//        console.log("form focused");
-//    });
-});
-$("#form1").on("focus", function() {
-    //$(this).value='';
-    console.log("form focused");
-});
+//$(document).ready(function(){
+//    console.log("hello world");
+////    $("#form1").focus(function() {
+////    $("#form1").on("focus", function() {
+////        //$(this).value='';
+////        console.log("form focused");
+////    });
+//});
+//$("#form1").on("focus", function() {
+//    //$(this).value='';
+//    console.log("form focused");
+//});
 
+document.getElementById("form1").onfocus=function() {
+    this.value = '';
+}
