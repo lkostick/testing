68660fdea0849b9cfca3366ce194d5ce0622dd2a
lkostick <lkostick1@gmail.com>
2018-10-10 11:16:56 -0400

    got it

--- level1.js
+++ level1.js
@@ -6,17 +6,17 @@
 ////        console.log("form focused");
 ////    });
 //});
-//$("#form1").on("focus", function() {
-//    //$(this).value='';
-//    console.log("form focused");
-//});
-window.onload = function() {
-    var element = document.getElementById("form1");
-    element.addEventListener("focus", myScript(element));
-//    document.getElementById("form1").onfocus=function() {
-//        this.value = '';
-//    };
-};
-function myScript(element) {
-    element.value = '';
-};
+$("#query").on("focus", function() {
+    $(this).value='';
+    console.log("form focused");
+});
+//window.onload = function() {
+//    var element = document.getElementById("form1");
+//    element.addEventListener("focus", myScript(element));
+////    document.getElementById("form1").onfocus=function() {
+////        this.value = '';
+////    };
+//};
+//function myScript(element) {
+//    element.elements[0].value = '';
+//};
